d27a8ff7339c0da4a8541d504b3ab2b62084bb62
Thanh-Vy Duong <dng.thanhvy@gmail.com>
2021-07-13 16:37:48 +0200

    add: profile

--- client/src/components/Profile/ProfileTop/ProfileTop.js
+++ client/src/components/Profile/ProfileTop/ProfileTop.js
@@ -1,15 +1,29 @@
+import { useState, useEffect } from 'react';
+
 import './ProfileTop.css';
+import { fetchUser } from '../../../api';
+
 import {Users} from '../../../data';
 
 export default function ProfileTop({ id, username, backgroundPicture, profilePicture }) {
 
-    const user = Users.find(elem => elem.id == id);
+    // const user = Users.find(elem => elem.id == id);
+    const [user, setUser] = useState({});
+
+    const fetchUserInfos = async(userId) => {
+        const res = (await fetchUser(userId)).data;
+        setUser(res);
+    }
+
+    useEffect(() => {
+        fetchUserInfos(id);
+    }, [])
 
     return (
         <div className="profileTop">
             <div className="profileTop__container">
-                <img src={`../../${user.backgroundPicture}`} alt="background" className="profileTop__userBackgroundImg"/>
-                <img src={`../../${user.profilePicture}`} alt="profile" className="profileTop__userProfileImg"/>
+                <img src={user.backgroundPicture} alt="background" className="profileTop__userBackgroundImg"/>
+                <img src={user.profilePicture} alt="profile" className="profileTop__userProfileImg"/>
                 <span className="profileTop__username">{user.username}</span>
             </div>
         </div>
